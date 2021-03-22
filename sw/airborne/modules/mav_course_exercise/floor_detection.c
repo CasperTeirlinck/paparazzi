969fdd13aefc645b4d0ea485de962864269da90e
Roelof123 <roelofstikker@hotmail.com>
2021-03-22 12:48:54 +0100

    added navigation code

--- sw/airborne/modules/mav_course_exercise/floor_detection.c
+++ sw/airborne/modules/mav_course_exercise/floor_detection.c
@@ -28,6 +28,8 @@
 #include "modules/mav_course_exercise/floor_detection.h"
 #include "modules/computer_vision/lib/vision/image.h"
 #include "subsystems/abi.h"
+#include "firmwares/rotorcraft/guidance/guidance_h.h"
+#include <stdio.h>
 
 #ifndef FLOOR_DETECT_FPS
 #define FLOOR_DETECT_FPS 0 ///< Default FPS (zero means run at camera fps)
@@ -37,6 +39,14 @@
 #define FLOOR_DETECT_TYPE 0 ///< 0 is simulation, 1 is real flight
 #endif
 
+#define FLOOR_DETECTION_VERBOSE TRUE
+
+#define PRINT(string,...) fprintf(stderr, "[floor_detection->%s()] " string,__FUNCTION__ , ##__VA_ARGS__)
+#if FLOOR_DETECTION_VERBOSE
+#define VERBOSE_PRINT PRINT
+#else
+#define VERBOSE_PRINT(...)
+#endif
 
 //TODO: If I remember correctly, teh camera FPS was decreased for testing puropses by Sunyou - Dani
 
@@ -52,6 +62,17 @@ struct YUV_color floor_real_max = {150, 133, 133};
 struct YUV_color floor_min;
 struct YUV_color floor_max;
 
+int vv1;
+int vv2;
+int vv3;
+int vv4;
+int vv5;
+int vv6;
+int vv7;
+int vv8;
+int vv9;
+int vv10;
+
 enum color_set {
     SIMU,
     REAL
@@ -63,9 +84,23 @@ static abi_event floor_detection_ev;
 static void floor_detection_test_cb(uint8_t __attribute__((unused)) sender_id,
                                int v1, int v2, int v3, int v4, int v5, int v6, int v7, int v8, int v9, int v10)
 {
-    /// I checked it working well!
-//    printf("%d, %d, %d, %d, %d, %d, %d, %d, %d, %d\n", v1,v2,v3,v4,v5,v6,v7,v8,v9,v10);
+//    I checked it working well!
+    printf("%d, %d, %d, %d, %d, %d, %d, %d, %d, %d\n", v1,v2,v3,v4,v5,v6,v7,v8,v9,v10);
+//
+vv1 = v1;
+vv2 = v2;
+vv3 = v3;
+vv4 = v4;
+vv5 = v5;
+vv6 = v6;
+vv7 = v7;
+vv8 = v8;
+vv9 = v9;
+vv10 = v10;
 }
+
+
+
 
 
 /// Bounds num between min and max.
@@ -154,10 +189,10 @@ void c_ground_obstacle_detect(struct image_t *input, int bottom_count, int certa
     }
 
     //TODO: This is here for testing purposes, deletet it for the flight version
-    for (int a=0; a<10; a++){
-        printf("%d ", obstacle_sector_array[a]);
-    }
-    printf("\n");
+//    for (int a=0; a<10; a++){
+//        printf("%d ", obstacle_sector_array[a]);
+//    }
+//    printf("\n");
 
 
     //publish the result via Abi. Passing 10 int is a stupid way but safe haha
@@ -211,8 +246,78 @@ enum navigation_state_t {
     OUT_OF_BOUNDS,
     REENTER_ARENA
 };
+float headingchange;
+float velocity;
+
 void floor_detection_periodic(void)
 {
+
+
+	int vvarray[10] = {vv1,vv2,vv3,vv4,vv5,vv6,vv7,vv8,vv9,vv10};
+
+
+
+	int holesize = 0;
+	int biggesthole = 0;
+	int biggestleft = 0;
+	int biggestright = 0;
+	int leftbound;
+	int rightbound;
+
+	for (int i=0;i<10;i++){
+		if (holesize == 0){// looking for the start of a hole, holesize of 0 means we are not in a hole
+			if (vvarray[i] == 0){//it's the left bound
+				leftbound = i;
+				holesize = holesize + 1;
+			}
+			}
+		else{
+			if (vvarray[i] == 0){
+				rightbound = i;
+				holesize = holesize + 1;
+				if (i==9){// if we are at the end of the data, we are also at the end of the hole
+					if (holesize > biggesthole){//found the new biggest hole
+									biggesthole=holesize;
+									biggestleft = leftbound;
+									biggestright = rightbound;
+								}
+								holesize = 0; //reset holesize
+			}
+		}
+			else{// found the end of a hole
+				if (holesize > biggesthole){//found the new biggest hole
+					biggesthole=holesize;
+					biggestleft = leftbound;
+					biggestright = rightbound;
+				}
+				holesize = 0; //reset holesize
+
+
+			}
+		}
+	}
+
+
+
+
+	if (biggesthole > 0){ //if there is a safe direction, turn to that direction
+		velocity = 0.3;
+		headingchange =( (biggestleft + biggestright)/2.f -4.5)/4.5 * 52.f;
+
+	}
+	else{// if there is no safe direction, stop and turn 60 degrees
+		velocity = 0.0;
+		headingchange  = 60.f; //now it always turns clockwise, maybe change
+	}
+
+	guidance_h_set_guided_body_vel(velocity,0);
+	guidance_h_set_guided_heading(RadOfDeg(headingchange)+stateGetNedToBodyEulers_f()->psi);
+
+	VERBOSE_PRINT("biggestleft,biggestright,biggesthole, headingchange: %d, %d, %d, %.2f\n",biggestleft,biggestright,biggesthole,headingchange);
+
+
+
+
     //TODO: For most of this, check the periodic in orange_avoider_guided
 
     //TODO: Decide on a safe distance from the drone, i.e. 1 meter
