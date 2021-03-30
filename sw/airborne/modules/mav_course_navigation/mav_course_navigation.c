249d1b8f4fa5b2917e30d3a6200060d915bb2b07
Roelof123 <roelofstikker@hotmail.com>
2021-03-30 22:32:40 +0200

    navigation now ignores unsafe columns if there are less than 5 adjacent. Also made some changes to make it a bit more cautious. Changed altitude in the flight plan to 1.5m

--- sw/airborne/modules/mav_course_navigation/mav_course_navigation.c
+++ sw/airborne/modules/mav_course_navigation/mav_course_navigation.c
@@ -40,7 +40,7 @@
 int len_view, width_drone, center_view;
 int thresh_front, green_max;
 int flag_front, flag_heading, flag_go;
-float velocity;
+float velocity = 0.0;
 float headingchange;
 int view_green[MT9F002_OUTPUT_HEIGHT];
 int view_line[MT9F002_OUTPUT_HEIGHT];
@@ -53,8 +53,10 @@ int latest_green_detection_message = 0;
 int latest_out_of_bounds_message = 0;
 int latest_edge_detection_message = 0;
 float integrated_velocity = 0;
-float loop_number = 0.0;
+float loop_number = 1.0;
 float average_velocity;
+int noise_threshold = 4;
+int consecutive_zero_velocity = 0;
 // ------------------------- NAVIGATION GLOBAL VARIABLES END-------------------------------------
 
 
@@ -73,7 +75,6 @@ static void floor_detection_cb(uint8_t __attribute__((unused)) sender_id, struct
           		printf(" %d", view_green[i]);
   }
 	printf("\n");
-//  printf("new green detection message %d\n",green_detection_message_number);
   green_detection_message_number++;
 
 }
@@ -88,26 +89,12 @@ static void edgebox_cb(uint8_t __attribute__((unused)) sender_id, struct obstacl
   for (int i = 0; i < MT9F002_OUTPUT_HEIGHT; i++) {
       view_line[i] = line_detection.x[i]; // View-analysis for line-detection
   }
-//  printf("Edge detection message %d\n",edge_detection_message_number);
   edge_detection_message_number++;
 
 
 
 }
 
-#ifndef OUTOFBOUNDS_ID
-#define OUTOFBOUNDS_ID ABI_BROADCAST
-#endif
-static abi_event outofbounds_ev;
-static void outofbounds_cb(uint8_t __attribute__((unused)) sender_id, float angle_rec)
-{
-  angle_bottom = angle_rec;
-  if (angle_bottom>0.1 || angle_bottom<-0.1){
-  printf("Out of bounds detection message %d, angle %f \n",out_of_bounds_message_number,angle_bottom);
-  }
-  out_of_bounds_message_number++;
-
-}
 
 // ------------------------- ABI COMMUNICATION END-------------------------------------
 
@@ -125,7 +112,6 @@ void mav_course_navigation_init(void)
 
   AbiBindMsgOBSTACLES(EDGEBOX_ID, &edgebox_ev, edgebox_cb);
 
-  AbiBindMsgOUTOFBOUNDS(OUTOFBOUNDS_ID, &outofbounds_ev, outofbounds_cb);
 }
 
 /*
@@ -135,37 +121,13 @@ void mav_course_navigation_init(void)
 
 void mav_course_navigation_periodic(void)
 {
-	// Only run the mudule if we are in the correct flight mode
+	// Only run the module if we are in the correct flight mode
 	  if (guidance_h.mode != GUIDANCE_H_MODE_GUIDED) {
 	    return;
 	  }
 
-	loop_number += 1.0f;
-	int number_of_obstructed_columns = 0;
 
-	for (int i = 0; i < MT9F002_OUTPUT_HEIGHT; i++) {
-		  if (view_line[i]){
-			  printf("%d ",i);
-		  number_of_obstructed_columns++;
-		  }
-	  }
-	if (number_of_obstructed_columns>0){
-					printf("\n Line detection detects %d obstructed columns\n", number_of_obstructed_columns);
-				}
 
-	// stop if bottom camera indicates out of bounds
-	// run this even if no new green_detection_message
-	if (angle_bottom > 90 && angle_bottom < 270 && out_of_bounds_message_number > latest_out_of_bounds_message){
-		latest_out_of_bounds_message = out_of_bounds_message_number;
-				velocity = 0.0;
-				headingchange = angle_bottom; //Could be minus
-//				guidance_h_set_guided_body_vel(velocity,0);
-//				guidance_h_set_guided_heading(RadOfDeg(headingchange)+stateGetNedToBodyEulers_f()->psi);
-				VERBOSE_PRINT("OUT OF BOUNDS DETECTED, would have turned %.2f degrees\n",headingchange);
-
-	}
-
-//	else{ //if no out of bounds detected
 		if (green_detection_message_number>latest_green_detection_message && edge_detection_message_number>latest_edge_detection_message){ // to prevent changing the heading multiple times for the same message
 			latest_green_detection_message = green_detection_message_number;
 			latest_edge_detection_message = edge_detection_message_number; 
@@ -177,19 +139,25 @@ void mav_course_navigation_periodic(void)
 			int rightbound;
 			int close_obstacle = 0;
 			int nothing_ahead = 1;
+			int noise_counter = 0;
+			int number_of_threats_ahead = 0;
 
-			//analyze data
+			// analyze data
+			// find biggest free space (hole) in vision
 			for (int i=0;i<MT9F002_OUTPUT_HEIGHT;i++){
 			  if (holesize == 0){// looking for the start of a hole, holesize of 0 means we are not in a hole
 				if ((view_green[i] == 0 || view_green[i] > 30) && view_line[i] == 0){//it's the left bound
 				  leftbound = i;
-				  holesize = holesize + 1;
+				  holesize = 1;
+				  noise_counter = 0; //the column is safe so reset noise counter
 				}
 				}
 			  else{
-				if ((view_green[i] == 0 || view_green[i] > 30) && view_line[i] == 0){
+
+				if ((view_green[i] == 0 || view_green[i] > 25) && view_line[i] == 0){// if the column is safe
 				  rightbound = i;
-				  holesize = holesize + 1;
+				  holesize = rightbound-leftbound;
+				  noise_counter = 0; //the column is safe so reset noise counter
 				  if (i==MT9F002_OUTPUT_HEIGHT-1){// if we are at the end of the data, we are also at the end of the hole
 					if (holesize > biggesthole){//found the new biggest hole
 							biggesthole=holesize;
@@ -200,31 +168,63 @@ void mav_course_navigation_periodic(void)
 				}
 			  }
 				else{// found the end of a hole
-				  if (holesize > biggesthole){//found the new biggest hole
-					biggesthole = holesize;
-					biggestleft = leftbound;
-					biggestright = rightbound;
-				  }
-				  holesize = 0; //reset holesize
+					if (holesize>0){// only increase noise counter if we are counting the size of a hole
+						noise_counter++;// run counter to check if it's just noise
+					}
+					if (noise_counter > noise_threshold){// if more than noise_threshold adjacent columns are unsafe
+						if (holesize > biggesthole){//found the new biggest hole
+							biggesthole = holesize;
+							biggestleft = leftbound;
+							biggestright = rightbound;
+						  }
+						holesize = 0; //reset holesize
+						noise_counter = 0;
+					}
+					else{
+						if (i==MT9F002_OUTPUT_HEIGHT-1){// if we are at the end of the data, and noise counter is below limit
+
+							rightbound = i;
+							holesize = rightbound - leftbound;
+									if (holesize > biggesthole){//found the new biggest hole
+											biggesthole=holesize;
+											biggestleft = leftbound;
+											biggestright = rightbound;
+										  }
+
+										}
+					}
 				}
 			  }
 
 
-			  if (i>0.4*MT9F002_OUTPUT_HEIGHT && i<0.6*MT9F002_OUTPUT_HEIGHT && view_green[i]<5 && view_green[i]>0){
-				  VERBOSE_PRINT("%d, ",i);
-				  close_obstacle = 1;
+			  if (i>0.35*MT9F002_OUTPUT_HEIGHT && i<0.65*MT9F002_OUTPUT_HEIGHT && view_green[i]<10 && view_green[i]>0){
+				  number_of_threats_ahead++;
+
 			  }
 			  if(i>0.3*MT9F002_OUTPUT_HEIGHT && i<0.7*MT9F002_OUTPUT_HEIGHT && view_green[i]>0){
 				  nothing_ahead = 0;
 			  }
+//			  VERBOSE_PRINT("index %d, view_green %d, holesize %d, bounds %d %d, noise_counter %d\n",i,view_green[i], holesize, leftbound,rightbound,noise_counter);
+			}
+//
+			if (number_of_threats_ahead > 20){
+				 close_obstacle = 1;
 			}
 
+			if (velocity<0.1){
+							consecutive_zero_velocity++;
+						}
+						else{
+							consecutive_zero_velocity=0;
+						}
+
 			//choose decision
-			if (biggesthole > 0.06*MT9F002_OUTPUT_HEIGHT && !close_obstacle){ //if there is a safe direction, turn to that direction
+			if (biggesthole > 0.1*MT9F002_OUTPUT_HEIGHT && consecutive_zero_velocity<20){ //if there is a safe direction, turn to that direction
 
 			  headingchange = (float)(0.5*(biggestleft+biggestright) - 259.5)/259.5 * 51.5;
-			  if (abs(headingchange) > 40){
+			  if (abs(headingchange) > 40 || close_obstacle){//set velocity to zero if the heading change is too big or if there is an obstacle ahead
 				  velocity = 0.0;
+
 			  }
 			  else if(abs(headingchange)>10){
 				  velocity = 0.3;
@@ -238,15 +238,18 @@ void mav_course_navigation_periodic(void)
 
 			}
 			else{// if there is no safe direction, stop and turn 60 degrees
-				if (close_obstacle){
-					VERBOSE_PRINT("\nCLOSE OBSTACLE, setting velocity to zero\n");
-				}
 			  velocity = 0.0;
-			  headingchange  = 60.f; //now it always turns clockwise, maybe change
+			  headingchange  = 60.f; //always turn in the same direction to avoid getting stuck in going back and forth
+			  if (consecutive_zero_velocity>=20){
+				  VERBOSE_PRINT("Turning because we're stuck");
+			  }
+			  consecutive_zero_velocity = 0;
 
 
 			}
-
+			if (close_obstacle){
+				VERBOSE_PRINT("\nCLOSE OBSTACLE, setting velocity to zero\n");
+			}
 
 
 
@@ -255,9 +258,11 @@ void mav_course_navigation_periodic(void)
 			guidance_h_set_guided_body_vel(velocity,0);
 			guidance_h_set_guided_heading(RadOfDeg(headingchange)+stateGetNedToBodyEulers_f()->psi);
 
-			integrated_velocity = integrated_velocity + velocity;
-			average_velocity = integrated_velocity/loop_number;
-			VERBOSE_PRINT("average velocity: %.1f\n",average_velocity);
+//			integrated_velocity = integrated_velocity + velocity;
+//			average_velocity = integrated_velocity/loop_number;
+//			loop_number += 1.0f;
+//			VERBOSE_PRINT("average velocity: %.1f\n",average_velocity);
+
 			VERBOSE_PRINT("left %d, right %d, biggesthole %d, location %.2f, headingchange %.2f, velocity %.2f\n",biggestleft,biggestright,biggesthole,(float)(0.5*(biggestleft+biggestright) - 259.5)/259.5,headingchange,velocity);
 			}
 //		}//end else statement
